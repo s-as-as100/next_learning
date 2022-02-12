640bc75d17e81fc827b073623e6906582f541a01
s-as-as100 <s.arifsaifi@gmail.com>
2022-02-12 11:34:25 +0530

    Navbar link component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,52 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
-// flow of next js project 
-// control go to package json file 
+import Link from "next/link";
+// flow of next js project
+// control go to package json file
 // entery point of next js  ---> _app.js
 // index. js file  is component same imported in app .js
 
-
 export default function Home() {
+const  styles = {
+  navcont : {
+    display:"flex",
+    backgroundColor:"red",
+
+     
+  },
+
+  ulList: { 
+    display:"flex",
+    width:"100%",
+    justifyContent:"space-between"
+  }
+}
+
+
   return (
     <>
-      <h1>Home Component</h1>
+      <nav  style={styles.navcont}>
+        <ul  style={styles.ulList}>
+          <Link href="/">
+            <a > Home </a>
+          </Link>
+          <Link href="/About">
+            <a> About</a>
+          </Link>
+          <Link href="/Contact">
+            <a>Contact</a> 
+          </Link>
+          <Link href="/blog" >
+            <a>Blog</a>
+          </Link>
+          <Link href="/Product" style={{
+            marginRight:"20px"
+          }}>
+            <a> Product</a>
+          </Link>
+        </ul>
+      </nav>
     </>
   );
 }
